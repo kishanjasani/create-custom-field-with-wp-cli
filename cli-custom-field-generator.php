a782df91b3161ed349caf1eaa250bb33dee127ad
Kishan Jasani <kishanjasani007@yahoo.in>
2020-09-03 18:24:17 +0530

    Add post generator with post title, taxonomie and meta data

--- cli-custom-field-generator.php
+++ cli-custom-field-generator.php
@@ -22,14 +22,85 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 */
 		public function generate_florplans( $args, $assoc_args ) {
 
-			$foo = $args[0];
-			$bar = $args[1];
-
 			$amount = $assoc_args['amount'];
 
-			WP_CLI::line( $foo );
-			WP_CLI::line( $bar );
-			WP_CLI::line( $amount );
+			$home_type_terms = get_terms(
+				[
+					'taxonomy'   => 'home_types',
+					'hide_empty' => false,
+				]
+			);
+
+			$home_type_ids = array_map(
+				function( $home_type ) {
+					return $home_type->term_id;
+				},
+				$home_type_terms
+			);
+
+			$home_feature_terms = get_terms(
+				[
+					'taxonomy'   => 'home_features',
+					'hide_empty' => false,
+				]
+			);
+
+			$home_feature_ids = array_map(
+				function( $home_feature ) {
+					return $home_feature->term_id;
+				},
+				$home_feature_terms
+			);
+
+			$facker = Faker\Factory::create();
+
+			$progress = \WP_CLI\Utils\make_progress_bar( 'Generating Floorplans', $amount );
+
+			for ( $i = 0; $i < $amount; $i++ ) {
+
+				$floorplan = wp_insert_post(
+					[
+						'post_title'  => 'The ' . $facker->firstName,
+						'post_status' => 'publish',
+						'post_type'   => 'floorplan',
+					]
+				);
+
+				wp_set_object_terms( $floorplan, array_rand( array_flip( $home_type_ids ), 1 ), 'home_types' );
+				wp_set_object_terms( $floorplan, array_rand( array_flip( $home_feature_ids ), 3 ), 'home_features' );
+
+				$possible_images = [ 48, 47, 34, 50, 30 ];
+
+				$image_keys = [
+					'featured_image', // 'featured_image',
+					'interior_1', // 'interior_1',
+					'interior_2', // 'interior_2',
+					'interior_3', // 'interior_3',
+					'interior_4', // 'interior_4',
+				];
+
+				foreach ( $image_keys as $key ) {
+					update_field( $key, array_rand( array_flip( $possible_images ), 1 ), $floorplan );
+				}
+
+				// Beds
+				update_field( 'field_5f50d8f4c4399', $facker->numberBetween( 2, 5 ), $floorplan );
+
+				// Bathroom
+				update_field( 'field_5f50d8e4c4398', $facker->numberBetween( 1, 5 ), $floorplan );
+
+				// square footage
+				update_field( 'field_5f50da0414601', $facker->numberBetween( 1500, 3000 ), $floorplan );
+
+				// Starting price
+				update_field( 'field_5f50d93a14600', $facker->numberBetween( 250000, 400000 ), $floorplan );
+
+				$progress->tick();
+			}
+
+			$progress->finish();
+			WP_CLI::success( $amount . ' Floorplans Generated!' );
+
 		}
 	}
 
